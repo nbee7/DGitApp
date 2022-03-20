fafe0ee0b79384980c087b76bf406e12aa9bb028
nbee7 <daffadit0721@gmail.com>
2022-03-20 16:05:38 +0700

    remove unused resource

--- app/src/main/java/com/submission/dicoding/dgitapp/utils/Constant.kt
+++ app/src/main/java/com/submission/dicoding/dgitapp/utils/Constant.kt
@@ -2,5 +2,4 @@ package com.submission.dicoding.dgitapp.utils
 
 object Constant {
     const val CONNECTION_TIMEOUT = 120L
-    const val DEBOUNCE_DURATION = 300L
 }
--- app/src/main/java/com/submission/dicoding/dgitapp/utils/DataMapper.kt
+++
@@ -1,6 +0,0 @@
-package com.submission.dicoding.dgitapp.utils
-
-import com.submission.dicoding.dgitapp.data.remote.response.UserItems
-
-object DataMapper {
-}
